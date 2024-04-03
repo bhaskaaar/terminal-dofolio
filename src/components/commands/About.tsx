8161878c9d8487b73c77c9ed50d8167b5d69268a
bhaskaaar <bhaskardatta321@gmail.com>
2024-04-03 14:45:36 +0530

    name takeover

--- src/components/commands/About.tsx
+++ src/components/commands/About.tsx
@@ -8,7 +8,7 @@ const About: React.FC = () => {
   return (
     <AboutWrapper data-testid="about">
       <p>
-        Hi, my name is <HighlightSpan>Sat Naing</HighlightSpan>!
+        Hi, my name is <HighlightSpan>Bhaskar Datta</HighlightSpan>!
       </p>
       <p>
         I'm <HighlightAlt>a full-stack developer</HighlightAlt> based in Yangon,
